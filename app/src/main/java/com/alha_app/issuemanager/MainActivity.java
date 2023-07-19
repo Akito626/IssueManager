89b0b978cfd7192c2cf0913ae5dd590b0d805961
Akito <al6.vha12@gmail.com>
2023-07-19 20:12:55 +0900

    closedのissueも表示するようにしました。また、更新ボタンも追加しました。

--- app/src/main/java/com/alha_app/issuemanager/MainActivity.java
+++ app/src/main/java/com/alha_app/issuemanager/MainActivity.java
@@ -1,5 +1,6 @@
 package com.alha_app.issuemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -9,15 +10,20 @@ import android.graphics.BitmapFactory;
 import android.graphics.BitmapShader;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -35,10 +41,16 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private IssueManager issueManager;
-    private ListView issueList;
-    private ArrayList<String> issueTitle;
-    private ArrayList<String> issueBodies;
+    private ListView openIssueList;
+    private ListView closedIssueList;
+    private ArrayList<String> openIssueTitle;
+    private ArrayList<String> openIssueBodies;
+    private ArrayList<String> closedIssueTitle;
+    private ArrayList<String> closedIssueBodies;
     private Handler handler;
+
+    TextView openText;
+    TextView closedText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,28 +62,74 @@ public class MainActivity extends AppCompatActivity {
 
         issueManager = (IssueManager) this.getApplication();
 
-        issueList = findViewById(R.id.issue_list);
+        openIssueList = findViewById(R.id.open_issuelist);
+        closedIssueList = findViewById(R.id.closed_issuelist);
         handler = new Handler();
 
-        issueTitle = new ArrayList<>();
-        issueBodies = new ArrayList<>();
+        openIssueTitle = new ArrayList<>();
+        openIssueBodies = new ArrayList<>();
+        closedIssueTitle = new ArrayList<>();
+        closedIssueBodies = new ArrayList<>();
 
-        issueList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        openIssueList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                issueManager.setIssueTitle(issueTitle.get(position));
-                issueManager.setIssueBody(issueBodies.get(position));
+                issueManager.setIssueTitle(openIssueTitle.get(position));
+                issueManager.setIssueBody(openIssueBodies.get(position));
+                startActivity(new Intent(getApplication(), EditorActivity.class));
+            }
+        });
+        closedIssueList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                issueManager.setIssueTitle(closedIssueTitle.get(position));
+                issueManager.setIssueBody(closedIssueBodies.get(position));
                 startActivity(new Intent(getApplication(), EditorActivity.class));
             }
         });
 
+        openText = findViewById(R.id.open);
+        openText.setOnClickListener(v -> {
+            closedIssueList.setVisibility(View.INVISIBLE);
+            openIssueList.setVisibility(View.VISIBLE);
+        });
+        closedText = findViewById(R.id.closed);
+        closedText.setOnClickListener(v -> {
+            openIssueList.setVisibility(View.INVISIBLE);
+            closedIssueList.setVisibility(View.VISIBLE);
+        });
+
         new Thread(() -> {
-            getIssues();
+            int i = getIssues("closed");
+            closedText.setText("Closed " + i);
+            i = getIssues("open");
+            openText.setText("Open " + i);
         }).start();
     }
 
-    public void getIssues(){
-        String urlString = BuildConfig.URL;
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_add) {
+            Toast.makeText(issueManager, "追加", Toast.LENGTH_SHORT).show();
+        } else if(item.getItemId() == R.id.action_update){
+            new Thread(() -> {
+                int i = getIssues("closed");
+                closedText.setText("Closed " + i);
+                i = getIssues("open");
+                openText.setText("Open " + i);
+            }).start();
+        }
+        return true;
+    }
+
+    public int getIssues(String s){
+        String urlString = BuildConfig.URL + "?state=" + s;
 
         String json = "";
         StringBuilder sb = new StringBuilder();
@@ -99,13 +157,22 @@ public class MainActivity extends AppCompatActivity {
                 tmp = tmp.substring(1, tmp.length()-1);
                 tmp = tmp.replaceAll("\\\\r", "");
                 tmp = tmp.replaceAll("\\\\n", "\n");
-                issueTitle.add(tmp);
+                if(s.equals("open")){
+                    openIssueTitle.add(tmp);
+                } else if(s.equals("closed")){
+                    closedIssueTitle.add(tmp);
+                }
+
                 item.put("title", tmp);
                 tmp = jsonResult.get(i).get("body").toString();
                 tmp = tmp.substring(1, tmp.length()-1);
                 tmp = tmp.replaceAll("\\\\r", "");
                 tmp = tmp.replaceAll("\\\\n", "\n");
-                issueBodies.add(tmp);
+                if(s.equals("open")){
+                    openIssueBodies.add(tmp);
+                } else if(s.equals("closed")){
+                    closedIssueBodies.add(tmp);
+                }
 
                 tmp = jsonResult.get(i).get("user").get("login").toString();
                 tmp = tmp.substring(1, tmp.length()-1);
@@ -128,17 +195,31 @@ public class MainActivity extends AppCompatActivity {
                 listData.add(item);
             }
 
-            handler.post(() -> {
-                issueList.setAdapter(new SimpleAdapter(
-                        this,
-                        listData,
-                        R.layout.issuelist_item,
-                        new String[] {"title", "date", "label", "name"},
-                        new int[] {R.id.title, R.id.date, R.id.label, R.id.name}
-                ));
-            });
+            if(s.equals("open")) {
+                handler.post(() -> {
+                    openIssueList.setAdapter(new SimpleAdapter(
+                            this,
+                            listData,
+                            R.layout.issuelist_item,
+                            new String[]{"title", "date", "label", "name"},
+                            new int[]{R.id.title, R.id.date, R.id.label, R.id.name}
+                    ));
+                });
+            } else if(s.equals("closed")){
+                handler.post(() -> {
+                    closedIssueList.setAdapter(new SimpleAdapter(
+                            this,
+                            listData,
+                            R.layout.issuelist_item,
+                            new String[]{"title", "date", "label", "name"},
+                            new int[]{R.id.title, R.id.date, R.id.label, R.id.name}
+                    ));
+                });
+            }
+            return jsonResult.size();
         } catch (Exception e){
             e.printStackTrace();
         }
+        return 0;
     }
 }
